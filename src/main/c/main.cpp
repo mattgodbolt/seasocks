18ab9c5bd381a8b88767f37aa89e90aded67b213
Matt Godbolt <mgodbolt@drwholdings.com>
2010-12-21 08:14:27 +0000

    [mg] Add a list of TODOs

--- src/main/c/main.cpp
+++ src/main/c/main.cpp
@@ -1,5 +1,15 @@
 #include "server.h"
 
+/*
+ * TODOs:
+ * * Add logging
+ * * Connection class does *everything*. Work out better division of labour; server should do more, connection should just be connection stuff, handlers for data?
+ * * sort out buffers and buffering
+ * * work out what state to hang on to; handle multiple WS endpoints
+ * * work out threading model for asynch message passing.
+ * * Fix bloody SO_REUSEADDR
+ */
+
 using namespace SeaSocks;
 
 int main(int argc, const char* argv[]) {
