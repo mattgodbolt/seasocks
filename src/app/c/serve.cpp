166a65f959eb2a4701c7d035bcf156002a1a1a02
Matt Godbolt <mgodbolt@drwholdings.com>
2011-04-27 07:26:38 -0500

    [mg] Add simple SSO support to bin/serve. No ACL yet though

--- src/app/c/serve.cpp
+++ src/app/c/serve.cpp
@@ -1,16 +1,41 @@
 #include "seasocks/printflogger.h"
 #include "seasocks/server.h"
+#include "seasocks/ssoauthenticator.h"
+
+#include <tclap/CmdLine.h>
+
 #include <boost/shared_ptr.hpp>
+#include <iostream>
 
 using namespace SeaSocks;
+using namespace TCLAP;
+
+static const char description[] =
+		"Serve static files over HTTP.";
 
 int main(int argc, const char* argv[]) {
-	if (argc != 3) {
-		std::cerr << "Usage: serve ROOT PORT" << std::endl;
-		return 1;
-	}
+	CmdLine cmd(description, ' ', "0.0");
+	ValueArg<int> portArg("p", "port", "Listen for incoming connections on PORT", false, 80, "PORT", cmd);
+	SwitchArg ssoArg("s", "enable-sso", "Protect content with Single Sign On", cmd);
+	SwitchArg verboseArg("v", "verbose", "Output verbose debug information", cmd);
+	ValueArg<std::string> ssoEnvironmentArg(
+			"e", "sso-environment", "Use SSO-ENV as the SSO environment (default PROD)",
+			false, "PROD", "SSO-ENV", cmd);
 
-	boost::shared_ptr<Logger> logger(new PrintfLogger(Logger::Level::INFO));
-	Server(logger).serve(argv[1], atoi(argv[2]));
+	UnlabeledValueArg<std::string> rootArg("serve-from-dir", "Use DIR as file serving root", true, "", "DIR", cmd);
+	cmd.parse(argc, argv);
+
+	boost::shared_ptr<Logger> logger(
+			new PrintfLogger(verboseArg.getValue() ? Logger::Level::DEBUG : Logger::Level::INFO));
+	Server server(logger);
+	if (ssoArg.getValue()) {
+		auto ssoOptions = SsoOptions::environment(ssoEnvironmentArg.getValue());
+		if (ssoOptions.authServer.size() == 0) {
+			std::cerr << "Unrecognized SSO environment '" << ssoEnvironmentArg.getValue() << "'" << std::endl;
+			return 1;
+		}
+		server.enableSingleSignOn(ssoOptions);
+	}
+	server.serve(rootArg.getValue().c_str(), portArg.getValue());
 	return 0;
 }
