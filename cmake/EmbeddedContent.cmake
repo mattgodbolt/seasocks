017fa4111f0931a012f411912452c10f60a5ee61
offa <bm-dev@yandex.com>
2015-10-13 21:39:10 +0200

    Travis builds using cmake added and cmake set to 2.8.x, since there's no v3.x availalbe on travis yet.

--- cmake/EmbeddedContent.cmake
+++ cmake/EmbeddedContent.cmake
@@ -8,7 +8,7 @@ file(GLOB EMBEDDED_FILES "src/main/web/*")
 
 add_custom_command(OUTPUT Embedded.cpp
                         COMMAND ${PYTHON} ${SCRIPT} ${EMBEDDED_FILES} $<ANGLE-R> Embedded.cpp
-                        COMMENT "Generate embedded content"
+                        COMMENT "Generating embedded content"
                         )
 
 add_library(embedded OBJECT Embedded.cpp)
