e2bb596818fda2dc1929d3d4019440ed7fb2ec19
Matt Godbolt <mgodbolt@drw.com>
2015-10-21 16:55:49 -0500

    Another attempt at fixing embedded pendantic compilation. Should address #16

--- scripts/gen_embedded.py
+++ scripts/gen_embedded.py
@@ -11,16 +11,27 @@ print """
 #include <unordered_map>
 
 namespace {
+"""
 
+files = []
+index = 1
+
+for f in sys.argv[1:]:
+    bytes = open(f, 'rb').read()
+    name = "fileData%d" % index
+    index += 1
+    files.append((name, os.path.basename(f), len(bytes)))
+    print 'const char %s[] = {' % name
+    for start in range(0, len(bytes), MAX_SLICE):
+        print '' + "".join(["'\\x%02x'," % ord(x) for x in bytes[start:start+MAX_SLICE]])
+    print '0 };'
+
+print """
 std::unordered_map<std::string, EmbeddedContent> embedded = {
 """
 
-for f in sys.argv[1:]:
-	bytes = open(f, 'rb').read()
-	print '{"/%s", {' % os.path.basename(f)
-        for start in range(0, len(bytes), MAX_SLICE):
-            print '"' + "".join(['\\x%02x' % ord(x) for x in bytes[start:start+MAX_SLICE]]) + '"'
-	print ',%d }},' % len(bytes)
+for name, base, length in files:
+    print '{"/%s", { %s, %d }},' % (base, name, length)
 
 print """
 };
