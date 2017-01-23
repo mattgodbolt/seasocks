d1074e633be0dc7be44bff2040d4c97451d6da66
offa <bm-dev@yandex.com>
2017-01-23 11:21:43 +0100

    Version fixed for all targets.

--- src/app/c/serve.cpp
+++ src/app/c/serve.cpp
@@ -23,8 +23,6 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
 // POSSIBILITY OF SUCH DAMAGE.
 
-#include "internal/Version.h"
-
 #include "seasocks/PrintfLogger.h"
 #include "seasocks/Server.h"
 #include "seasocks/StringUtil.h"
