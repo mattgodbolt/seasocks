cdb3c9b15c69d0365acb73ec94e96da8ee1e0221
offa <bm-dev@yandex.com>
2017-03-17 10:42:49 +0100

    Type deduction fixed (#66).

---
+++ src/test/c/ResponseBuilderTests.cpp
@@ -0,0 +1,27 @@
+
+#include "seasocks/ResponseBuilder.h"
+#include "catch.hpp"
+
+using namespace seasocks;
+
+TEST_CASE("appendIntValue", "[ResponseBuilderTests]") {
+    ResponseBuilder builder;
+    builder << int{3};
+    auto result = std::dynamic_pointer_cast<SynchronousResponse>(builder.build());
+    CHECK_THAT(result->payload(), Equals("3"));
+}
+
+TEST_CASE("appendCStringValue", "[ResponseBuilderTests]") {
+    ResponseBuilder builder;
+    const char* cStr = "abc";
+    builder << cStr;
+    auto result = std::dynamic_pointer_cast<SynchronousResponse>(builder.build());
+    CHECK_THAT(result->payload(), Equals("abc"));
+}
+
+TEST_CASE("appendStringValue", "[ResponseBuilderTests]") {
+    ResponseBuilder builder;
+    builder << std::string("xyz");
+    auto result = std::dynamic_pointer_cast<SynchronousResponse>(builder.build());
+    CHECK_THAT(result->payload(), Equals("xyz"));
+}
