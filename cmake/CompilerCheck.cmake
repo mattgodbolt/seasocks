72bf82b4327e9da7128f7df27afe42a881630a05
offa <bm-dev@yandex.com>
2017-09-08 17:20:05 +0200

    Setting the C++ Standard using CMAKE_CXX_STANDARD and let CMake figure out which flag is necessary for the used compiler.

--- cmake/CompilerCheck.cmake
+++ cmake/CompilerCheck.cmake
@@ -1,18 +1,3 @@
-include(CheckIncludeFileCXX)
-include(CheckCXXCompilerFlag)
-set(TEMP_DIR "${CMAKE_BINARY_DIR}/temp")
-
-# C++0x / C++11
-CHECK_CXX_COMPILER_FLAG("-std=c++1y" COMPILER_SUPPORTS_CXX1Y)
-CHECK_CXX_COMPILER_FLAG("-std=c++11" COMPILER_SUPPORTS_CXX11)
-CHECK_CXX_COMPILER_FLAG("-std=c++0x" COMPILER_SUPPORTS_CXX0X)
-if (COMPILER_SUPPORTS_CXX1Y)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++1y")
-elseif (COMPILER_SUPPORTS_CXX11)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
-elseif(COMPILER_SUPPORTS_CXX0X)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
-else()
-    message(FATAL_ERROR "The compiler ${CMAKE_CXX_COMPILER} has no C++11 support. Please use a different C++ compiler.")
-endif ()
-
+set(CMAKE_CXX_STANDARD 11)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
