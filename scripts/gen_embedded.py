f1b040482cdab7519da52666d46a38f0cba7f823
offa <bm-dev@yandex.com>
2018-07-13 16:26:17 +0200

    Workaround for the python 2/3 compatibility.

--- scripts/gen_embedded.py
+++ scripts/gen_embedded.py
@@ -2,7 +2,15 @@
 
 import os, os.path, sys
 
+
 MAX_SLICE = 70
+
+def as_byte(data):
+    if sys.version_info < (3,):
+        return ord(data)
+    else:
+        return data
+
 
 print("""
 #include "internal/Embedded.h"
@@ -23,7 +31,7 @@ for f in sys.argv[1:]:
     files.append((name, os.path.basename(f), len(bytes)))
     print('const char %s[] = {' % name)
     for start in range(0, len(bytes), MAX_SLICE):
-        print('' + "".join(["'\\x%02x'," % x for x in bytes[start:start+MAX_SLICE]]))
+        print('' + "".join(["'\\x%02x'," % as_byte(x) for x in bytes[start:start+MAX_SLICE]]))
     print('0 };')
 
 print("""
