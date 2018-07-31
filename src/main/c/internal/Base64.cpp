27f9781b7702db1500510c8c94dc571761ae854d
offa <bm-dev@yandex.com>
2018-07-31 18:20:57 +0200

    Consts added.

--- src/main/c/internal/Base64.cpp
+++ src/main/c/internal/Base64.cpp
@@ -34,12 +34,12 @@ namespace seasocks {
 
 std::string base64Encode(const void* data, size_t length) {
     std::string output;
-    auto dataPtr = reinterpret_cast<const uint8_t*>(data);
+    const auto dataPtr = reinterpret_cast<const uint8_t*>(data);
     for (auto i = 0u; i < length; i += 3) {
-        auto bytesLeft = length - i;
-        auto b0 = dataPtr[i];
-        auto b1 = bytesLeft > 1 ? dataPtr[i + 1] : 0;
-        auto b2 = bytesLeft > 2 ? dataPtr[i + 2] : 0;
+        const auto bytesLeft = length - i;
+        const auto b0 = dataPtr[i];
+        const auto b1 = bytesLeft > 1 ? dataPtr[i + 1] : 0;
+        const auto b2 = bytesLeft > 2 ? dataPtr[i + 2] : 0;
         output.push_back(cb64[b0 >> 2]);
         output.push_back(cb64[((b0 & 0x03) << 4) | ((b1 & 0xf0) >> 4)]);
         output.push_back((bytesLeft > 1 ? cb64[((b1 & 0x0f) << 2) | ((b2 & 0xc0) >> 6)] : '='));
