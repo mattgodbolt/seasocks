4a8e647023926facd2725b39e5ac3de86ff52a85
A.J.Bauer <a.j.bauer@hotmail.com>
2018-12-04 22:31:34 +0100

    Update HybiPacketDecoder.h

--- src/main/c/internal/HybiPacketDecoder.h
+++ src/main/c/internal/HybiPacketDecoder.h
@@ -32,45 +32,38 @@
 #include <string>
 #include <vector>
 
-using namespace std;
-
 namespace seasocks {
 
-	class HybiPacketDecoder {
-		Logger& _logger;
-		const std::vector<uint8_t>& _buffer;
-		size_t _messageStart;
+class HybiPacketDecoder {
+    Logger& _logger;
+    const std::vector<uint8_t>& _buffer;
+    size_t _messageStart;
+public:
+    HybiPacketDecoder(Logger& logger, const std::vector<uint8_t>& buffer);
 
-	public:
-		HybiPacketDecoder(Logger& logger, const std::vector<uint8_t>& buffer);
+    enum class Opcode : uint8_t {
+        Cont = 0x0,  // Deprecated in latest hybi spec, here anyway (and used).
+        Text = 0x1,
+        Binary = 0x2,
+        Close = 0x8,
+        Ping = 0x9,
+        Pong = 0xA,
+    };
 
-		enum class Opcode : uint8_t {
-			Cont = 0x0,  // Deprecated in latest hybi spec, here anyway.
-			Text = 0x1,
-			Binary = 0x2,
-			Close = 0x8,
-			Ping = 0x9,
-			Pong = 0xA,
-		};
+    enum class MessageState {
+        NoMessage,
+        TextMessage,
+        BinaryMessage,
+        TextMessageFragment,
+        BinaryMessageFragment,
+        Error,
+        Ping,
+        Pong,
+        Close
+    };
+    MessageState decodeNextMessage(std::vector<uint8_t>& messageOut, bool& deflateNeeded, Opcode& firstOpcodeFinunset);
 
-		enum class MessageState {
-			NoMessage,
-			TextMessage,
-			BinaryMessage,
-			TextMessageFragment,
-			BinaryMessageFragment,
-			Error,
-			Ping,
-			Pong,
-			Close,
-			ChunksStart,
-			ChunksNext,
-			ChunksEnd
-
-		};
-		MessageState decodeNextMessage(std::vector<uint8_t>& messageOut, bool& deflateNeeded, Opcode& firstOpcodeFinunset);
-		
-		size_t numBytesDecoded() const;
-	};
+    size_t numBytesDecoded() const;
+};
 
 }
