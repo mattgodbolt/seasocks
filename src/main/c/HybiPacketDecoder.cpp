520e40d0a3c9bbca43ec89485a89bd43a1450b53
A.J.Bauer <a.j.bauer@hotmail.com>
2018-12-05 08:48:51 +0100

    Update HybiPacketDecoder.cpp

--- src/main/c/HybiPacketDecoder.cpp
+++ src/main/c/HybiPacketDecoder.cpp
@@ -40,55 +40,49 @@ HybiPacketDecoder::HybiPacketDecoder(Logger& logger,
 }
 
 HybiPacketDecoder::MessageState HybiPacketDecoder::decodeNextMessage(
-        std::vector<uint8_t>& messageOut, bool& deflateNeeded, Opcode& firstOpcodeFinunset) {
+        std::vector<uint8_t>& messageOut, bool& deflateNeeded, uint8_t& firstOpcodeFinunset) {
     if (_messageStart + 1 >= _buffer.size()) {
-      return MessageState::NoMessage;
+        return MessageState::NoMessage;
     }
 
-    bool finset = (_buffer[_messageStart] & 0x80) == 0x80;
-
-    auto reservedBits = _buffer[_messageStart] & (7 << 4);
+    auto reservedBits = _buffer[_messageStart] & (7<<4);
     if ((reservedBits & 0x30) != 0) {
-      LS_WARNING(&_logger, "Received hybi frame with reserved bits set - error");
-      return MessageState::Error;
+        LS_WARNING(&_logger, "Received hybi frame with reserved bits set - error");
+        return MessageState::Error;
     }
 
     deflateNeeded = !!(reservedBits & 0x40);
 
     auto opcode = static_cast<Opcode>(_buffer[_messageStart] & 0xf);
-    if (!finset && opcode != Opcode::Cont) {
-      firstOpcodeFinunset = opcode;
-    }   
-
     size_t payloadLength = _buffer[_messageStart + 1] & 0x7fu;
     auto maskBit = _buffer[_messageStart + 1] & 0x80;
     auto ptr = _messageStart + 2;
     if (payloadLength == 126) {
-      if (_buffer.size() < 4) { return MessageState::NoMessage; }
-      uint16_t raw_length;
-      memcpy(&raw_length, &_buffer[ptr], sizeof(raw_length));
-      payloadLength = htons(raw_length);
-      ptr += 2;
-    }
-    else if (payloadLength == 127) {
-      if (_buffer.size() < 10) { return MessageState::NoMessage; }
-      uint64_t raw_length;
-      memcpy(&raw_length, &_buffer[ptr], sizeof(raw_length));
-      payloadLength = (size_t)__bswap_64(raw_length);
-      ptr += 8;
+        if (_buffer.size() < 4) { return MessageState::NoMessage; }
+        uint16_t raw_length;
+        memcpy(&raw_length, &_buffer[ptr], sizeof(raw_length));
+        payloadLength = htons(raw_length);
+        ptr += 2;
+    } else if (payloadLength == 127) {
+        if (_buffer.size() < 10) { return MessageState::NoMessage; }
+        uint64_t raw_length;
+        memcpy(&raw_length, &_buffer[ptr], sizeof(raw_length));
+        payloadLength = __bswap_64(raw_length);
+        ptr += 8;
     }
     uint32_t mask = 0;
     if (maskBit) {
-      // MASK is set.
-      if (_buffer.size() < ptr + 4) { return MessageState::NoMessage; }
-      uint32_t raw_length;
-      memcpy(&raw_length, &_buffer[ptr], sizeof(raw_length));
-      mask = htonl(raw_length);
-      ptr += 4;
+        // MASK is set.
+        if (_buffer.size() < ptr + 4) { return MessageState::NoMessage; }
+        uint32_t raw_length;
+        memcpy(&raw_length, &_buffer[ptr], sizeof(raw_length));
+        mask = htonl(raw_length);
+        ptr += 4;
     }
     else
     {
-      return MessageState::Error;
+        LS_WARNING(&_logger, "Received unmasked hybi frame from client ");
+        return MessageState::Error;
     }
     auto bytesLeftInBuffer = _buffer.size() - ptr;
     if (payloadLength > bytesLeftInBuffer) { return MessageState::NoMessage; }
@@ -96,14 +90,23 @@ HybiPacketDecoder::MessageState HybiPacketDecoder::decodeNextMessage(
     messageOut.clear();
     messageOut.reserve(payloadLength);
     for (auto i = 0u; i < payloadLength; ++i) {
-      auto byteShift = (3 - (i & 3)) * 8;
-      messageOut.push_back(static_cast<uint8_t>((_buffer[ptr++] ^ (mask >> byteShift)) & 0xff));
+        auto byteShift = (3 - (i & 3)) * 8;
+        messageOut.push_back(static_cast<uint8_t>((_buffer[ptr++] ^ (mask >> byteShift)) & 0xff));
     }
     _messageStart = ptr;
 
+    bool finset = (_buffer[_messageStart] & 0x80) == 0x80;
+    if (!finset && opcode != Opcode::Cont) {
+        firstOpcodeFinunset = (uint8_t)opcode;
+    }
+
     switch (opcode) {
+    default:
+        LS_WARNING(&_logger, "Received hybi frame with unknown opcode "
+                             << static_cast<int>(opcode));
+        return MessageState::Error;
     case Opcode::Cont:
-      switch (firstOpcodeFinunset)
+      switch ((Opcode)firstOpcodeFinunset)
       {
       case Opcode::Text:
         if (finset) {
@@ -145,15 +148,11 @@ HybiPacketDecoder::MessageState HybiPacketDecoder::decodeNextMessage(
       }
       
     case Opcode::Ping:
-      return MessageState::Ping;
+        return MessageState::Ping;
     case Opcode::Pong:
-      return MessageState::Pong;
+        return MessageState::Pong;
     case Opcode::Close:
-      return MessageState::Close;
-    default:
-      LS_WARNING(&_logger, "Received hybi frame with unknown opcode "
-        << static_cast<int>(opcode));
-      return MessageState::Error;
+        return MessageState::Close;
     }
 }
 
