d79eaa3822aec70efc182b2a35b39b6f42925219
Ray Burgemeestre <rayb@nvidia.com>
2022-08-22 15:33:05 +0200

    MD5: Removed null pointer subtraction
    Credits to Maxim Sharabayko who fixed it in the following PR in one of
    his repositories: https://github.com/Haivision/srt/pull/2392

--- src/main/c/md5/md5.cpp
+++ src/main/c/md5/md5.cpp
@@ -79,6 +79,7 @@
 #include "md5/md5.h"
 
 #include <cstring>
+#include <cstdint>
 
 #undef BYTE_ORDER /* 1 = big-endian, -1 = little-endian, 0 = unknown */
 #ifdef ARCH_IS_BIG_ENDIAN
@@ -187,7 +188,7 @@ md5_process(md5_state_t* pms, const md5_byte_t* data /*[64]*/) {
          * On little-endian machines, we can process properly aligned
          * data without copying it.
          */
-            if (!((data - (const md5_byte_t*) 0) & 3)) {
+            if (!(uintptr_t(data) & 3)) {
                 /* data are properly aligned */
                 X = (const md5_word_t*) data;
             } else {
