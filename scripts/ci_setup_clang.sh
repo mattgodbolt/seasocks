769da9ea243d23e91f2ec5a07edcf939c702e13c
offa <bm-dev@yandex.com>
2022-08-26 15:36:33 +0200

    Migrate CI build to Docker images
    Additionally, use CodeCov Action and update setup-python
    and use 'ubuntu-latest'

--- scripts/ci_setup_clang.sh
+++ scripts/ci_setup_clang.sh
@@ -2,10 +2,8 @@
 
 VERSION=$1
 
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-add-apt-repository "deb http://apt.llvm.org/focal/ llvm-toolchain-focal-${VERSION} main"
 apt update
-apt-get install -y clang-${VERSION} libc++-${VERSION}-dev libc++abi-${VERSION}-dev
+apt-get install -y libc++-${VERSION}-dev libc++abi-${VERSION}-dev
 
 if [[ ${VERSION} -ge 12 ]]; then
     apt-get install -y --no-install-recommends libunwind-${VERSION}-dev
